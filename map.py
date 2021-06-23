610719e65f995b3262f25db4272a9c5415d535ca
Isha Sharma <isha.sha14@gmail.com>
2021-06-23 13:48:51 -0700

    Add color labels for type of service at bank and add comments

--- map.py
+++ map.py
@@ -5,23 +5,43 @@ import pandas as pd
 my_map = folium.Map(location=[43.66430664668449, -79.39241763253044],
                     zoom_start=15)
 
-# sample Marker for Map
-# folium.Marker(location=[43.64668848, -79.3800808],
-#               popup=folium.Popup('<b>RBC Branch and ATM: 200 Bay Street<b>',
-#                                  max_width=100),
-#               icon=folium.Icon(icon="cloud"),).add_to(my_map)
-
+# read banks.csv and convert it into a Dataframe
 df = pd.read_csv('banks.csv')
 
+# convert the Dataframe into a dictionary of dictionaries, with keys being the
+# row number/index of the Dataframe and values being dictionaries (keys are
+# column labels, such as Name, Address, Latitude, etc., and values are the
+# corresponding value)
 banks_dict = df.to_dict('index')
 
+# loop through banks_dict
 for i in range(df.shape[0]):
-    folium.Marker(location=[banks_dict[i]['Latitude'],
-                            banks_dict[i]['Longitude']],
-                  popup=folium.Popup('<b>{} - {}<b>'.format(banks_dict[i]['Name'],
-                                                            banks_dict[i]['Address']),
-                                     max_width=100),
-                  icon=folium.Icon(icon="university", prefix="fa"),).add_to(my_map)
+    # if the bank has a branch and an ATM at the current location, then color it blue (default)
+    if banks_dict[i]['Branch'] == 'Y' and banks_dict[i]['ATM'] == 'Y':
+        folium.Marker(location=[banks_dict[i]['Latitude'],
+                                banks_dict[i]['Longitude']],
+                      popup=folium.Popup('<b>{} - {}<b>'.format(banks_dict[i]['Name'],
+                                                                banks_dict[i]['Address']),
+                                         max_width=100),
+                      icon=folium.Icon(icon="university", prefix="fa"),).add_to(my_map)
+
+    # if the bank only has an ATM at the current location, then color it purple
+    elif banks_dict[i]['Branch'] == 'N' and banks_dict[i]['ATM'] == 'Y':
+        folium.Marker(location=[banks_dict[i]['Latitude'],
+                                banks_dict[i]['Longitude']],
+                      popup=folium.Popup('<b>{} - {}<b>'.format(banks_dict[i]['Name'],
+                                                                banks_dict[i]['Address']),
+                                         max_width=100),
+                      icon=folium.Icon(color="purple", icon="university", prefix="fa"),).add_to(my_map)
+
+    # if the bank only has a branch at the current location, then color it orange
+    else:
+        folium.Marker(location=[banks_dict[i]['Latitude'],
+                                banks_dict[i]['Longitude']],
+                      popup=folium.Popup('<b>{} - {}<b>'.format(banks_dict[i]['Name'],
+                                                                banks_dict[i]['Address']),
+                                         max_width=100),
+                      icon=folium.Icon(color="orange", icon="university", prefix="fa"),).add_to(my_map)
 
 # renders Map
 my_map.save("index.html")
