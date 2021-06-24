0cac8012d1b98d9fea203a3564bce66cfb854833
Isha Sharma <isha.sha14@gmail.com>
2021-06-24 11:44:57 -0700

    Add HTML formatting for accessibility services and style popups

--- map.py
+++ map.py
@@ -18,29 +18,74 @@ banks_dict = df.to_dict('index')
 for i in range(df.shape[0]):
     # if the bank has a branch and an ATM at the current location, then color it blue (default)
     if banks_dict[i]['Branch'] == 'Y' and banks_dict[i]['ATM'] == 'Y':
+        html = """
+           <h4> {} - {} </h4>
+           <span><i> Branch Facilities: </i></span>
+           <ul>
+                <li> Wheelchair Accessible: {} </li>
+                <li> Barrier-Free Safe Deposit Boxes: {} </li>
+                <li> Tablets for ASL: {} </li>
+           </ul>
+           <span><i> ATM Facilities: </i></span>
+           <ul>
+                <li> Wheelchair Accessible: {} </li>
+                <li> Voice-Enabled: {} </li>
+                <li> Braille: {} </li>
+           </ul>
+           """.format(banks_dict[i]['Name'],
+                      banks_dict[i]['Address'],
+                      banks_dict[i]['Wheelchair Accessible'],
+                      banks_dict[i]['Barrier-free safe deposit boxes'],
+                      banks_dict[i]['Tablets for ASL'],
+                      banks_dict[i]['Wheelchair Accessible.1'],
+                      banks_dict[i]['Voice-enabled'],
+                      banks_dict[i]['Braille'])
         folium.Marker(location=[banks_dict[i]['Latitude'],
                                 banks_dict[i]['Longitude']],
-                      popup=folium.Popup('<b>{} - {}<b>'.format(banks_dict[i]['Name'],
-                                                                banks_dict[i]['Address']),
-                                         max_width=100),
+                      popup=folium.Popup(html=html,
+                                         max_width=250),
                       icon=folium.Icon(icon="university", prefix="fa"),).add_to(my_map)
 
     # if the bank only has an ATM at the current location, then color it purple
     elif banks_dict[i]['Branch'] == 'N' and banks_dict[i]['ATM'] == 'Y':
+        html = """
+           <h4> {} - {} </h4>
+           <span><i> ATM Facilities: </i></span>
+           <ul>
+                <li> Wheelchair Accessible: {} </li>
+                <li> Voice-Enabled: {} </li>
+                <li> Braille: {} </li>
+           </ul>
+           """.format(banks_dict[i]['Name'],
+                      banks_dict[i]['Address'],
+                      banks_dict[i]['Wheelchair Accessible.1'],
+                      banks_dict[i]['Voice-enabled'],
+                      banks_dict[i]['Braille'])
         folium.Marker(location=[banks_dict[i]['Latitude'],
                                 banks_dict[i]['Longitude']],
-                      popup=folium.Popup('<b>{} - {}<b>'.format(banks_dict[i]['Name'],
-                                                                banks_dict[i]['Address']),
-                                         max_width=100),
+                      popup=folium.Popup(html=html,
+                                         max_width=250),
                       icon=folium.Icon(color="purple", icon="university", prefix="fa"),).add_to(my_map)
 
     # if the bank only has a branch at the current location, then color it orange
     else:
+        html = """
+           <h4> {} - {} </h4>
+           <span><i> Branch Facilities: </i></span>
+           <ul>
+                <li> Wheelchair Accessible: {} </li>
+                <li> Barrier-Free Safe Deposit Boxes: {} </li>
+                <li> Tablets for ASL: {} </li>
+           </ul>
+           """.format(banks_dict[i]['Name'],
+                      banks_dict[i]['Address'],
+                      banks_dict[i]['Wheelchair Accessible'],
+                      banks_dict[i]['Barrier-free safe deposit boxes'],
+                      banks_dict[i]['Tablets for ASL'])
         folium.Marker(location=[banks_dict[i]['Latitude'],
                                 banks_dict[i]['Longitude']],
-                      popup=folium.Popup('<b>{} - {}<b>'.format(banks_dict[i]['Name'],
-                                                                banks_dict[i]['Address']),
-                                         max_width=100),
+                      popup=folium.Popup(html=html,
+                                         max_width=250),
                       icon=folium.Icon(color="orange", icon="university", prefix="fa"),).add_to(my_map)
 
 # renders Map
